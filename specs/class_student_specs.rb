da9d1acb9b25801076b521260b8d6c714d1f35d6
Jonathan Lavi <jonathan.lavi@gmail.com>
2018-11-26 15:36:33 +0000

    add student cohort setter method

--- specs/class_student_specs.rb
+++ specs/class_student_specs.rb
@@ -5,18 +5,22 @@ require_relative ('../class_student.rb')
 
 class TestStudent < Minitest::Test
 
-def test_initialise_student
-  student = Student.new('Jonathan', 'E27')
-  assert_equal('Jonathan', student.name)
-  assert_equal('E27', student.cohort)
-end
+  def test_initialise_student
+    student = Student.new('Jonathan', 'E27')
+    assert_equal('Jonathan', student.name)
+    assert_equal('E27', student.cohort)
+  end
 
-def test_set_name
-  student = Student.new('Rick', 'E27')
-  student.name = 'Jonathan'
-  assert_equal('Jonathan', student.name)
-end
+  def test_set_name
+    student = Student.new('Rick', 'E27')
+    student.name = 'Jonathan'
+    assert_equal('Jonathan', student.name)
+  end
 
-
+  def test_set_cohort
+    student = Student.new('Rick', 'G5')
+    student.cohort = 'E27'
+    assert_equal('E27', student.cohort)
+  end
 
 end
