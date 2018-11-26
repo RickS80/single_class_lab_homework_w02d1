f06c008cfd6a447e49879856c711465bd2039423
Jonathan Lavi <jonathan.lavi@gmail.com>
2018-11-26 15:28:44 +0000

    first commit, create student class

---
+++ specs/class_student_specs.rb
@@ -0,0 +1,16 @@
+require ('minitest/autorun')
+require ('minitest/rg')
+
+require_relative ('../class_student.rb')
+
+class TestStudent < Minitest::Test
+
+def test_initialise_student
+  student = Student.new('Jonathan', 'E27')
+  assert_equal('Jonathan', student.name)
+  assert_equal('E27', student.cohort)
+end
+
+
+
+end
