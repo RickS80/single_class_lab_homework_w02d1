8297f4b56ac6d3c17a3dee03f453fb7308bc7fe2
Jonathan Lavi <jonathan.lavi@gmail.com>
2018-11-26 16:22:28 +0000

    Add points property to class

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -11,6 +11,7 @@ class TestTeam < Minitest::Test
     assert_equal("Jaguars", team.name)
     assert_equal(players, team.players)
     assert_equal("Ferguson", team.coach)
+    assert_equal(0, team.points)
   end
 
   def test_set_team_coach
@@ -37,14 +38,12 @@ class TestTeam < Minitest::Test
     players = ["Smith", "Jackson", "Jordan", "Yaoming", "O'Neill"]
     team = Team.new("Jaguars", players, "Ferguson")
     assert_equal(true, team.player_in_team("Jordan"))
-
   end
 
   def test_player_in_team__does_not_exist
     players = ["Smith", "Jackson", "Jordan", "Yaoming", "O'Neill"]
     team = Team.new("Jaguars", players, "Ferguson")
     assert_equal(false, team.player_in_team("Kowalski"))
-
   end
 
 end
